3022b771ee32875161dfa5a47f8bcd9343aa7273
Robert Gonia <robertgonia@Roberts-MBP.fios-router.home>
2020-12-03 09:46:16 -0500

    Added cartIcon and CartDropdown to our app. Added reducers.

---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,6 @@
+// import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import CartActionTypes from "./cart.types";
+
+export const toggleCartHidden = () => ({
+  type: CartActionTypes.TOGGLE_CART_HIDDEN,
+});
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/user.reducer";
+import cartReduceer from "./cart/cart.reducer";
 
 export default combineReducers({
   user: userReducer,
+  cart: cartReduceer,
 });
