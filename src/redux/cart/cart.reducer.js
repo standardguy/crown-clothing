ad1f8de1675a1818946745220c8fae91110cca07
Robert Gonia <robertgonia@Roberts-MBP.fios-router.home>
2020-12-03 14:59:22 -0500

    Updating app with reselect.  Replaced cart selecters with memoized selectors

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from "./cart.types";
-import { addItemToCart, addITemToCart } from "./cart.utils";
+import { addItemToCart } from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
