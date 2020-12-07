e5e8eb496f95e38e59ab63496fc37051f5dd4532
Robert Gonia <robertgonia@Roberts-MBP.fios-router.home>
2020-12-07 14:49:55 -0500

    Added Diretory and Shop data to our redux store using redux-persist

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import { selectCollections } from "../../redux/shop/shop.selectors";
+
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
-import SHOP_DATA from "./shop.data.js";
+const ShopPage = ({ collections }) => (
+  <div className="shop_page">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
 
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop_page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
