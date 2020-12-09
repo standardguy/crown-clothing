7d50909e246d5e01b10a75db1c0e4b4c4dfc04f8
Robert Gonia <robertgonia@Roberts-MBP.fios-router.home>
2020-12-09 13:57:08 -0500

    Fixed the /shop page by adding a selector to change the collection obj to an array

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,11 +1,17 @@
 import { createSelector } from "reselect";
 import memoize from "lodash.memoize";
+import { Obj } from "prelude-ls";
 
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
+);
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
 );
 
 export const selectCollection = memoize((collectionUrlParam) =>
